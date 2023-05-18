a5ba162b0a3ca458b367f78c485fc19e674ff22c
Stephanie de Castro Silva <stephanie.silva@yaman.com.br>
2023-05-18 20:03:46 -0300

    'v2'

--- galeria/models.py
+++ galeria/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 
 class Fotografia(models.Model):
+
+    opcoes_categoria = [
+        ("NEBULOSA", "Nebulosa"),
+        ("ESTRELA", 'Estrela'),
+        ("GALÁXIA", "Galáxia"),
+        ("PLANETA", "Planeta"),
+    ]
     nome = models.CharField(max_length=100, null=False, blank=False)
     legenda = models.CharField(max_length=150, null=False, blank=False)
+    categoria = models.CharField(max_length=100, choices=opcoes_categoria, null=False, default='')
     descricao = models.TextField(null=False, blank=False)
     foto = models.CharField(max_length=100, null=False, blank=False)
 
